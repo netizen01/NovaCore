cc7f7f546159c38733af314320555d2a91b780bd
Joel Stewart <joel@codeinversion.com>
2016-06-07 19:45:17 -0500

    Nit

--- Source/Easing.swift
+++ Source/Easing.swift
@@ -7,7 +7,7 @@ import Foundation
 
 public class Easing {
     
-    public class func sineInOut(time time:Double, start:Double, delta:Double, duration:Double) -> Double {
+    public class func sineInOut(time time: Double, start: Double, delta: Double, duration: Double) -> Double {
         return -delta / 2 * (cos(M_PI * time / duration) - 1) + start
     }
     
