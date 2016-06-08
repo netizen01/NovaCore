0546d60a18493574da05ed62e310409da836daed
Joel Stewart <joel@codeinversion.com>
2016-06-07 19:46:49 -0500

    Add lighter / darker / brightness UIColor ext

--- Source/UIColor.swift
+++ Source/UIColor.swift
@@ -6,7 +6,7 @@
 import UIKit
 
 extension UIColor {
-        
+    
     public func pixel() -> UIImage {
         let rect = CGRectMake(0, 0, 1, 1)
         UIGraphicsBeginImageContext(rect.size)
@@ -30,4 +30,24 @@ extension UIColor {
         return image
     }
     
+    public func lighter(amount : CGFloat = 0.25) -> UIColor {
+        return hueColorWithBrightnessAmount(1 + amount)
+    }
+    
+    public func darker(amount : CGFloat = 0.25) -> UIColor {
+        return hueColorWithBrightnessAmount(1 - amount)
+    }
+    
+    public func hueColorWithBrightnessAmount(amount: CGFloat) -> UIColor {
+        var hue: CGFloat = 0
+        var saturation: CGFloat = 0
+        var brightness: CGFloat = 0
+        var alpha: CGFloat = 0
+        
+        if getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
+            return UIColor(hue: hue, saturation: saturation, brightness: brightness * amount, alpha: alpha)
+        } else {
+            return self
+        }
+    }
 }
