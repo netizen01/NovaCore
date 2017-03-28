62e85c1d95b639b86985e871ca8a05f64ac9fcca
Joel Stewart <joel@codeinversion.com>
2017-03-28 09:49:02 -0500

    Use Swifty Pi

--- Source/Easing.swift
+++ Source/Easing.swift
@@ -8,7 +8,7 @@ import Foundation
 open class Easing {
     
     open class func sineInOut(time: Double, start: Double, delta: Double, duration: Double) -> Double {
-        return -delta / 2 * (cos(M_PI * time / duration) - 1) + start
+        return -delta / 2 * (cos(.pi * time / duration) - 1) + start
     }
     
 }
