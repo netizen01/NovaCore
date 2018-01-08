2ff0fb1af44eff4ab737ac46295d8f4eebddc99e
Netizen01 <joel@codeinversion.com>
2018-01-08 17:25:34 -0600

    Adding some common stuffs

--- Source/UIButton.swift
+++ Source/UIButton.swift
@@ -7,6 +7,17 @@ import UIKit
 
 extension UIButton {
     
+    public func templateImages() {
+        templateImageForState(.normal)
+        templateImageForState(.selected)
+        templateImageForState(.highlighted)
+        templateImageForState(.disabled)
+    }
+    
+    public func templateImageForState(_ state: UIControlState) {
+        setImage(image(for: state)?.withRenderingMode(.alwaysTemplate), for: state)
+    }
+    
     public func setBackgroundColor(_ color: UIColor, forState state: UIControlState) {
         setBackgroundImage(color.pixel(), for: state)
     }
